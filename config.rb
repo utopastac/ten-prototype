943c522b91757be28fa063a8119bcfcdb8182eec
Peter Wright <utopastac@gmail.com>
2018-01-04 17:45:48 +0000

    Switch to Mastercard

--- config.rb
+++ config.rb
@@ -22,7 +22,7 @@ activate :directory_indexes
 activate :sprockets
 activate :breadcrumbs, separator: '  ', wrapper: :li
 
-config[:client] = "cnb"
+config[:client] = "mastercard"
 #mastercard
 #ten_private
 #fake_brand
